f4ed45d66da0b7df149de480f5f54f0e7e894ef9
Aarya Paradkar <aaru7276@gmail.com>
2024-01-16 21:21:15 +0530

    ikj

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'bis', 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
