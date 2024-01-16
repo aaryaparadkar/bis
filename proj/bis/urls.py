1f6e9bd75b2c1bb423f93c300e65c85d086cb4df
Aarya Paradkar <aaru7276@gmail.com>
2024-01-16 21:16:15 +0530

    wreiu

--- proj/bis/urls.py
+++ proj/bis/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path
 from bis.views import index
 
-app_name = 'construction'
+app_name = 'bis'
 urlpatterns = [
     path('', index, name='index')
 ]
