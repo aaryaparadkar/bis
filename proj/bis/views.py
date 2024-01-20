2307215965fbaffb9d9a47fa21242727d896230d
Aarya Paradkar <aaru7276@gmail.com>
2024-01-20 21:49:06 +0530

    working

--- proj/bis/views.py
+++ proj/bis/views.py
@@ -15,17 +15,8 @@ def index(request):
         contact.subject = subject
         contact.save()
         return HttpResponse("<h3>Thank you!</h3>")
-
-    return render(request, 'bis/index.html')
-
-def history(request):
-    try:
-        history_content = History.objects.latest('id')
-    except ObjectDoesNotExist:
-        history_content = None
-    
-    return render(request, 'bis/index.html', {'history_content': history_content})
-
-def contactus(request):
+        
     contact_info_list = ContactUs.objects.all()
-    return render(request, 'bis/index.html', {'contact_info_list': contact_info_list})
+    history_content = History.objects.latest('id')
+
+    return render(request, 'bis/index.html', {'history_content': history_content, 'contact_info_list': contact_info_list})
