428cec2c3b2786ecaaa97b2cf923b14b0c524b43
Aarya Paradkar <aaru7276@gmail.com>
2024-01-25 17:45:26 +0530

    hello ji

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
